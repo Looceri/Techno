92f190b562f8e42948be43dce835b13def8bf8dc
Loocer <142151699+Looceri@users.noreply.github.com>
2024-05-23 13:21:43 +0200

    c

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,9 +20,6 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -30,7 +27,19 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+                        @if (Route::currentRouteName() != 'login' && Route::currentRouteName() != 'register')
+                            @if (Route::has('tecnologias.create') && Route::currentRouteName() != 'tecnologias.create')
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('tecnologias.create') }}">Criar</a>
+                                </li>
+                            @endif
 
+                            @if (Route::has('tecnologias.index') && Route::currentRouteName() != 'tecnologias.index')
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('tecnologias.index') }}">Listar</a>
+                                </li>
+                            @endif
+                        @endif
                     </ul>
 
                     <!-- Right Side Of Navbar -->
